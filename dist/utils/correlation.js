20554f7816e4726ec23ebb9e3fe45e6dfd8b9e59
Noe Juarez <noe_juarez@epam.com>
2022-06-12 01:15:26 -0500

    [Main] PHI Function with array destructuring

--- dist/utils/correlation.js
+++ dist/utils/correlation.js
@@ -2,12 +2,10 @@ import JOURNAL from './JOURNAL.js';
 function addEntry(events, squirrel) {
     JOURNAL.push({ events, squirrel });
 }
-function phi(table) {
-    return (table[3] * table[0] - table[2] * table[1]) /
-        Math.sqrt((table[2] + table[3]) *
-            (table[0] + table[1]) *
-            (table[1] + table[3]) *
-            (table[0] + table[2]));
+function phi([n00, n01, n10, n11]) {
+    return (n11 * n00 - n10 * n01) /
+        Math.sqrt((n10 + n11) * (n00 + n01) *
+            (n01 + n11) * (n00 + n10));
 }
 function tableFor(event, journal) {
     let table = [0, 0, 0, 0];
--- utils/correlation.ts
+++ utils/correlation.ts
@@ -4,13 +4,11 @@ function addEntry(events: string[], squirrel: boolean) {
     JOURNAL.push({events, squirrel});
   }
   
-function phi(table: number[]): number {
-    return (table[3] * table[0] - table[2] * table[1]) /
-        Math.sqrt((table[2] + table[3]) *
-                (table[0] + table[1]) *
-                (table[1] + table[3]) *
-                (table[0] + table[2]));
-}
+  function phi([n00, n01, n10, n11]: number[]): number {
+    return (n11 * n00 - n10 * n01) /
+      Math.sqrt((n10 + n11) * (n00 + n01) *
+                (n01 + n11) * (n00 + n10));
+  }
 
 function tableFor(event, journal): number[] {
     let table: number[] = [0, 0, 0, 0];
