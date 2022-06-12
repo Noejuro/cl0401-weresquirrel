5a3b6048d4a8702678459431dd86ac80965a079b
Noe Juarez <noe_juarez@epam.com>
2022-06-12 01:12:38 -0500

    [HotFix] Syntax

--- dist/utils/correlation.js
+++ dist/utils/correlation.js
@@ -23,13 +23,10 @@ function tableFor(event, journal) {
 }
 function journalEvents(journal) {
     let events = [];
-    for (let entry of journal) {
-        for (let event of entry.events) {
-            if (!events.includes(event)) {
+    for (let entry of journal)
+        for (let event of entry.events)
+            if (!events.includes(event))
                 events.push(event);
-            }
-        }
-    }
     return events;
 }
 export { addEntry, phi, tableFor, journalEvents };
--- utils/correlation.ts
+++ utils/correlation.ts
@@ -15,23 +15,20 @@ function phi(table: number[]): number {
 function tableFor(event, journal): number[] {
     let table: number[] = [0, 0, 0, 0];
     for (let entry of journal) {
-    let index: number = 0;
-    if (entry.events.includes(event)) index += 1;
-    if (entry.squirrel) index += 2;
-    table[index] += 1;
+        let index: number = 0;
+        if (entry.events.includes(event)) index += 1;
+        if (entry.squirrel) index += 2;
+        table[index] += 1;
     }
     return table;
 }
 
 function journalEvents(journal): string[] {
     let events: string[] = [];
-    for (let entry of journal) {
-    for (let event of entry.events) {
-        if (!events.includes(event)) {
-        events.push(event);
-        }
-    }
-    }
+    for (let entry of journal)
+        for (let event of entry.events)
+            if (!events.includes(event))
+                events.push(event);
     return events;
 }
 
